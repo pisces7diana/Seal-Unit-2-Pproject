3fd0bd84b46d40a1c73f792ca1db4116a40e0fed
pisces7diana <email7diana@gmail.com>
2024-01-19 23:57:43 -0800

    Project 2 done as best as I could

--- controllers/expense.js
+++ controllers/expense.js
@@ -43,8 +43,8 @@ router.get("/seed", async (req, res) => {
         // array of dummy Expenses
          const dummyExpenses = [
              {merchant: "Panda Express", date: "January 1, 2024", price: 1, paymentMethod: "cash", category: "Food", notes: "break from work", requestedRefund: false},
-             {merchant: "Chick-fil-A", date: "January 2, 2024", price: 3, paymentMethod: "1234", category: "Drinks", notes: "break from school", requestedRefund: false},
-             {merchant: "Chilli's", date: "January 3, 2024", price: 5, paymentMethod: "cash", category: "Food", notes: "test", requestedRefund: false},
+             {merchant: "Chick-fil-A", date: "January 2, 2024", price: 3, paymentMethod: "1234", category: "Food", notes: "break from school", requestedRefund: false},
+             {merchant: "Chilli's", date: "January 3, 2024", price: 5, paymentMethod: "cash", category: "Drinks", notes: "Hang out", requestedRefund: false},
              {merchant: "Walmart", date: "January 4, 2024", price: 7, paymentMethod: "9010", category: "Clothes", notes: "nothing", requestedRefund: false},
              {merchant: "Rent", date: "January 5, 2024", price: 9, paymentMethod: "0001", category: "Home", notes: "Husband", requestedRefund: false},
          ];
---
+++ scratch.js
@@ -0,0 +1,32 @@
+// // Login Page
+
+// // A place to keep track of your expenses!
+// //
+
+// Welcome back!
+// // Login
+
+// First time here?
+// // Sign up
+
+
+// <a href="/expenses">Expenses Index</a>
+// <a href="/expenses/new">Add a new Expense</a>
+
+
+// <nav>
+//     <a href="/user/signup"><button>Signup</button></a>
+//     <a href="/user/login"><button>Login</button></a>
+//     <a href="/user/logout"><button>Log Out</button></a>
+// </nav>
+
+
+
+// form {
+//     margin-top: 400px;
+//     margin-left: 600px;
+//     padding: 20px;
+//     border: 3px solid black;
+//     background-color: white;
+//     width:fit-content;
+// }
